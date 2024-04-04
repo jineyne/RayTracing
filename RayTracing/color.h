031b6833f90cb3aa193230bd16d804e875cfe704
Park_Chul_Jin <61934809+elzpdldb2005@users.noreply.github.com>
2024-04-04 16:54:13 +0900

    Add Utility classes and functions

---
+++ RayTracing/color.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+#include "vec3.h"
+
+using color = vec3;
+
+void write_color(std::ostream &out, color pixel_color);
--- RayTracing/main.cpp
+++ RayTracing/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+#include "color.h"
+#include "vec3.h"
+
 int main(int argc, char* argv[])
 {
     // Image
@@ -12,19 +15,16 @@ int main(int argc, char* argv[])
 
     for (int y = 0; y < imageHeight; ++y)
     {
+        std::clog << "\rScanlines remaining: " << (imageHeight - y) << ' ' << std::flush;
+        
         for (int x = 0; x < imageWidth; ++x)
         {
-            auto r = double(x) / (imageWidth - 1);
-            auto g = double(y) / (imageHeight - 1);
-            auto b = 0;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            std::cout << ir << " " << ig << " " << ib << "\n";
+            auto pixel_color = color(double(x)/(imageWidth-1), double(y)/(imageHeight-1), 0);
+            write_color(std::cout, pixel_color);
         }
     }
+
+    std::clog << "\rDone.                 \n";
     
     return 0;
 }
